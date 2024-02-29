86bc73fa75c78d799ee698da6741e11ca55c7613
ajay121212github <ajayrajpatidar121212@gmail.com>
2024-02-29 14:33:25 +0530

    3 commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,7 +3,9 @@ export const sendToken = (user , statusCode ,res , message) =>{
     const options = {
         expires: new Date(Date.now() + (5 * 24 * 60 * 60 * 1000)), // 5 days from now
         httpOnly: true , 
-        secure: true
+        secure: true ,
+        sameSite: 'None',
+        
     };
     
 
